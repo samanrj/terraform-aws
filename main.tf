129e4bf11485db56a40b3ea8db886158d4a24920
Saman Rajaei <saman_persis_rj@yahoo.com>
2020-12-20 01:01:32 +0000

    ALB needs to be attached to min 2 subnets so adding that, adding tfstate.backup to gitignore

--- main.tf
+++ main.tf
@@ -125,62 +125,64 @@ resource "aws_lb_listener" "front_end" {
 
 
 
-# resource "aws_autoscaling_group" "web-asg" {
-#   availability_zones   = local.availability_zones
-#   name                 = "terraform-example-asg"
-#   max_size             = var.asg_max
-#   min_size             = var.asg_min
-#   desired_capacity     = var.asg_desired
-#   force_delete         = true
-#   launch_configuration = aws_launch_configuration.web-lc.name
-#   load_balancers       = [aws_lb.front_end.name]
-#
-#   #vpc_zone_identifier = ["${split(",", var.availability_zones)}"]
-#   tag {
-#     key                 = "Name"
-#     value               = "web-asg"
-#     propagate_at_launch = "true"
-#   }
-# }
-#
-# resource "aws_launch_configuration" "web-lc" {
-#   name          = "terraform-example-lc"
-#   image_id      = var.aws_amis[var.aws_region]
-#   instance_type = var.instance_type
-#
-#   # Security group
-#   security_groups = [aws_security_group.default.id]
-#   # user_data       = file("userdata.sh")
-#   # key_name        = var.key_name
-# }
-#
-# # Our default security group to access
-# # the instances over SSH and HTTP
-# resource "aws_security_group" "default" {
-#   name        = "terraform_example_sg"
-#   description = "Used in the terraform"
-#
-#   # SSH access from anywhere
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   # HTTP access from anywhere
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   # outbound internet access
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_autoscaling_group" "web-asg" {
+  availability_zones   = local.availability_zones
+  name                 = "terraform-example-asg"
+  max_size             = var.asg_max
+  min_size             = var.asg_min
+  desired_capacity     = var.asg_desired
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.web-lc.name
+  target_group_arns    = [aws_lb_target_group.front_end.arn]   # ===> https://github.com/terraform-aws-modules/terraform-aws-autoscaling/issues/16
+
+  # load_balancers       = [aws_lb.front_end.name]
+  #vpc_zone_identifier = ["${split(",", var.availability_zones)}"]
+
+  tag {
+    key                 = "Name"
+    value               = "web-asg"
+    propagate_at_launch = "true"
+  }
+}
+
+resource "aws_launch_configuration" "web-lc" {
+  name          = "terraform-example-lc"
+  image_id      = var.aws_amis[var.aws_region]
+  instance_type = var.instance_type
+
+  # Security group
+  security_groups = [aws_security_group.default.id]
+  # user_data       = file("userdata.sh")
+  # key_name        = var.key_name
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_example_sg"
+  description = "Used in the terraform"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
